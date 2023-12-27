8990a9d46997c68093bf991ab2315182c4c00d06
MuradIsayev <muradisayevazer@gmail.com>
2023-12-27 22:52:43 +0400

    methods updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,4 @@
+// app.controller.ts
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SendMessageDto } from './users/dto/send-message.dto';
 import { AppService } from './app.service';
@@ -11,8 +12,18 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @Post('keys')
+  async generateKeys() {
+    return await this.appService.generateKeyPair();
+  }
+
   @Post('generate-session-key')
   async generateSessionKey(@Body() sendMessageDto: SendMessageDto) {
     return await this.appService.generateSessionKey(sendMessageDto);
+  }
+
+  @Post('decrypt-message')
+  async decryptMessage(@Body() sendMessageDto: SendMessageDto) {
+    return await this.appService.decryptMessage(sendMessageDto);
   }
 }
